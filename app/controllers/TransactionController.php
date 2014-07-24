bb36db1292e6a9860b45342b7a9c7c310b06795e
Jared Loosli <jloosli@gmail.com>
2014-07-24 02:05:20 -0600

    Transaction Classes cleaned up

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -1,23 +1,22 @@
 <?php
 
-class TransactionController extends BaseController
-{
+class TransactionController extends BaseController {
 
     /**
      * Display a listing of the resource.
      *
      * @return Response
      */
-    public function index($bank_id, $user_id) {
+    public function index( $bank_id, $user_id ) {
         $transactions = Transaction::with( 'envelope_transaction' )->where( 'user_id', $user_id )->paginate();
 
         return $transactions;
     }
 
-    public function user($id) {
-        $transactions = Transaction::with('envelope_transaction')->where('user_id','=',$id)->get();
+    public function user( $id ) {
+        $transactions = Transaction::with( 'envelope_transaction' )->where( 'user_id', '=', $id )->get();
 
-        return Response::json($transactions);
+        return Response::json( $transactions );
     }
 
     /**
@@ -28,54 +27,55 @@ class TransactionController extends BaseController
      *
      * @return Response
      */
-    public function store($bank_id, $user_id) {
-        $mainTransaction = Input::get('transaction');
-        $envelope_transactions = Input::get('envelope_transactions');
-        $transaction_check = array_reduce(
+    public function store( $bank_id, $user_id ) {
+        $user = User::where( 'bank_id', $bank_id )->where( 'id', $user_id )->first();
+        if ( !$user ) {
+            throw new Symfony\Component\HttpKernel\Exception\NotFoundHttpException( "User not found" );
+        }
+        $mainTransaction       = Input::get( 'transaction' );
+        $envelope_transactions = Input::get( 'envelope_transactions' );
+        $transaction_check     = array_reduce(
             $envelope_transactions,
-            function($item1, $item2) {
+            function ( $item1, $item2 ) {
                 return $item1 - $item2['amount'];
-        },
-            $mainTransaction['amount']);
-        if($transaction_check != 0) {
-            throw new Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Envelope Transactions don't match up");
+            },
+            $mainTransaction['amount'] );
+        if ( $transaction_check != 0 ) {
+            throw new Symfony\Component\HttpKernel\Exception\BadRequestHttpException( "Envelope Transactions don't match up" );
         }
 
-        $user = User::where('bank_id', $bank_id)->where('id', $user_id)->first();
-        if(!$user) {
-            throw new Symfony\Component\HttpKernel\Exception\NotFoundHttpException("User not found");
-        }
         try {
-            DB::transaction(function () use ($user, $mainTransaction, $envelope_transactions) {
+            DB::transaction( function () use ( $user, $mainTransaction, $envelope_transactions ) {
                 // Update the user balance
-                $user->balance+=$mainTransaction['amount'];
+                $user->balance += $mainTransaction['amount'];
                 $user->save();
 
                 // Enter the envelope transaction
-                $transaction = new Transaction();
-                $transaction->amount = $mainTransaction['amount'];
+                $transaction              = new Transaction();
+                $transaction->amount      = $mainTransaction['amount'];
                 $transaction->description = $mainTransaction['description'];
-                $user->transactions()->save($transaction);
+                $user->transactions()->save( $transaction );
 
                 // Pull out the envelope_transactions and save the overall transaction
                 // Save the envelope Transactions
-                foreach ($envelope_transactions as $et) {
-                    $theSubTransaction = new EnvelopeTransaction();
-                    $theSubTransaction->amount = $et['amount'];
+                foreach ( $envelope_transactions as $et ) {
+                    $theSubTransaction              = new EnvelopeTransaction();
+                    $theSubTransaction->amount      = $et['amount'];
                     $theSubTransaction->envelope_id = $et['envelope_id'];
-                    $transaction->envelope_transaction()->save($theSubTransaction);
+                    $transaction->envelope_transaction()->save( $theSubTransaction );
 
                     // Update the existing envelope balances.
-                    $envelope = Envelope::findorFail($et['envelope_id']);
+                    $envelope = Envelope::findorFail( $et['envelope_id'] );
                     $envelope->balance += $et['amount'];
                     $envelope->save();
                 }
-            });
-            return Response::api()->withArray(array('success'=>true));
+            } );
 
-        } catch (Exception $e) {
-            var_dump($e);
-            throw new Dingo\Api\Exception\StoreResourceFailedException("Couldn't save Transaction");
+            return Response::api()->withArray( array( 'success' => true ) );
+
+        } catch ( Exception $e ) {
+            var_dump( $e );
+            throw new Dingo\Api\Exception\StoreResourceFailedException( "Couldn't save Transaction" );
         }
     }
 
@@ -84,17 +84,21 @@ class TransactionController extends BaseController
      *
      * @param $bank_id
      * @param $user_id
-     * @param $trans_id
+     * @param $trans_ids
      *
      * @return Transaction
      */
-    public function show($bank_id, $user_id, $trans_id) {
-        $transaction = Transaction::with( 'envelope_transaction' )
-            ->where( 'user_id', $user_id )
-            ->where('id', $trans_id)
-            ->first();
+    public function show( $bank_id, $user_id, $trans_ids ) {
+        $trans_ids    = explode( ',', $trans_ids );
+        $transactions = Transaction::with( 'envelope_transaction' )
+                                   ->where( 'user_id', $user_id )
+                                   ->whereIn( 'id', $trans_ids )
+                                   ->get();
+        if ( count( $transactions ) === 0 ) {
+            throw new Symfony\Component\HttpKernel\Exception\NotFoundHttpException( "Transaction(s) not found" );
+        }
 
-        return $transaction;
+        return $transactions;
     }
 
 }
--- app/tests/TransactionTest.php
+++ app/tests/TransactionTest.php
@@ -39,29 +39,57 @@ class TransactionTest extends TestCase {
         $this->checkJsonResponse( 200, $response );
     }
 
-    public function testCreateUserMissingInfo() {
-        $user     = [
-            'name'     => 'Bob',
-            'username' => 'bob',
-            'password' => 'pass'
+    public function testCreateTransactionWrongUser() {
+        $notGoingToBeUsed = [
+            'amount' => 35
         ];
-        $response = $this->call( 'POST', '/api/banks/1/users', $user );
-        $this->checkJsonResponse( 422, $response );
+        $response         = $this->call( 'POST', '/api/banks/1/users/400/transactions', $notGoingToBeUsed );
+        $this->checkJsonResponse( 404, $response );
     }
 
-    public function testGetIndividuals() {
+    public function testCreateTransactionBadMissingInfo() {
         $this->seed();
+        // Transaction and envelopeTransactions don't add up
+        $transaction = [
+            'transaction'           => [
+                'amount'      => 50,
+                'description' => "Wanted to get some lollypops",
+            ],
+            'envelope_transactions' => [
+                [
+                    'envelope_id' => 1,
+                    'amount'      => 10,
+                ],
+                [
+                    'envelope_id' => 2,
+                    'amount'      => 21.57 - 35,
+                ],
+                [
+                    'envelope_id' => 3,
+                    'amount'      => 25,
+                ]
+            ]
+        ];
+        $response    = $this->call( 'POST', '/api/banks/1/users/1/transactions', $transaction );
+        $this->checkJsonResponse( 400, $response );
+    }
 
-        $response = $this->call( 'GET', '/api/banks/1/users/1,2' );
+    public function testGetIndividualTransactions() {
+        $this->seed();
+        $this->testCreateTransaction();
+        $this->testCreateTransaction();
+        $this->testCreateTransaction();
+
+        $response = $this->call( 'GET', '/api/banks/1/users/1/transactions/1,2' );
 
         $this->checkJsonResponse( 200, $response );
 
-        $this->assertContains( 'users', $response->getContent() );
-        $this->assertContains( 'First User', $response->getContent() );
+        $this->assertContains( 'transactions', $response->getContent() );
+        $this->assertContains( 'lollypops', $response->getContent() );
     }
 
-    public function testGetMissingIndividuals() {
-        $response = $this->call( 'GET', '/api/banks/1/users/800' );
+    public function testGetMissingTransactions() {
+        $response = $this->call( 'GET', '/api/banks/1/users/1/transactions/450' );
         $this->checkJsonResponse( 404, $response );
     }
 
