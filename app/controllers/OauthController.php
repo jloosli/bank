e0b49226adfef78c1011313053f07241a3e4388b
Jared Loosli <jloosli@gmail.com>
2014-07-29 11:20:55 -0600

    Code cleanup

--- app/controllers/OauthController.php
+++ app/controllers/OauthController.php
@@ -5,28 +5,6 @@ use Illuminate\Support\Facades\Redirect;
 
 class OauthController extends \BaseController {
 
-//    /**
-//     * @param $oauth_provider
-//     * @param $credentials
-//     *
-//     * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
-//     * @return string
-//     */
-//    protected function storeCredentials( $oauth_provider, $credentials ) {
-//        $user = \AvantiDevelopment\JrBank\User::where( 'email', $credentials['email'] )->first();
-//        if ( !$user ) {
-//            throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException(
-//                sprintf( "No users with the email address of %s are set up.", $credentials['email'] )
-//            );
-//        }
-//        $userAuth                 = new \AvantiDevelopment\JrBank\Oauth();
-//        $userAuth->oauth_provider = $oauth_provider;
-//        $userAuth->oauth_uid      = $credentials['id'];
-//        $user->oauth()->save( $userAuth );
-//
-//        return $userAuth->token;
-//    }
-
     public function loginWithGoogle() {
         // get data from input
         $code = Input::get( 'code' );
