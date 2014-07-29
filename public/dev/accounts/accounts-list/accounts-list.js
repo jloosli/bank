4163b2c8de09a9f155dda77a8c73395efc103388
Jared Loosli <jloosli@gmail.com>
2014-07-29 15:46:43 -0600

    Updated img directory

--- public/dev/accounts/accounts-list/accounts-list.js
+++ public/dev/accounts/accounts-list/accounts-list.js
@@ -1,4 +1,8 @@
-angular.module('jrbank').controller('AccountsListCtrl',function($scope){
+(function () {
+    "use strict";
 
+    function AccountsListCtrl () {
 
-});
+    }
+    angular.module('jrbank').controller('AccountsListCtrl', AccountsListCtrl);
+})();
