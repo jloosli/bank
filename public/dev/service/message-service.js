452b84794d90cc82319b1d93e7046df53465cacb
Jared Loosli <jloosli@gmail.com>
2014-07-31 15:52:46 -0600

    Added message service

--- public/dev/service/message-service.js
+++ public/dev/service/message-service.js
@@ -2,9 +2,55 @@
     /*
      @ngInject
      */
-    function messageService() {
+    function messageService($timeout) {
         "use strict";
         var service = {};
+        var me = this;
+
+        var messages = [];
+
+        function guid() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
+        }
+
+        service.addMessage = function (theMessage) {
+            /*
+             Messages format: { id, message, type, duration, persist }
+             */
+            var id = guid();
+            var message = {
+                id: id,
+                message: theMessage.message || "",
+                type: theMessage.type || 'normal',
+                persist: !!theMessage.persist || false
+            };
+            messages.push(message);
+            if(theMessage.duration) {
+                $timeout(function() {
+                    me.removeMessage(id);
+                }, theMessage.duration);
+            }
+            return id;
+        };
+
+        service.removeMessage = function(id) {
+            messages = _.remove(messages, function(msg) {
+                return msg.id === id;
+            });
+        };
+
+        service.messages = function() {
+            return messages;
+        };
+
+        service.init = function() {
+            messages = _.remove(messages, function(msg) {
+                return !msg.persist;
+            });
+        };
 
         return service;
 
