322c7c1b7e9f0cf13730c15c869b1248bfa2ae3b
Jared Loosli <jloosli@gmail.com>
2014-07-31 15:35:35 -0600

    Added Message service

---
+++ public/dev/service/message-service-spec.js
@@ -0,0 +1,11 @@
+describe('messageService', function() {
+
+  beforeEach(module('jrbank'));
+
+  it('should ...', inject(function(messageService) {
+
+	//expect(messageService.doSomething()).toEqual('something');
+
+  }));
+
+});
---
+++ public/dev/service/message-service.js
@@ -0,0 +1,14 @@
+(function() {
+    /*
+     @ngInject
+     */
+    function messageService() {
+        "use strict";
+        var service = {};
+
+        return service;
+
+    }
+    angular.module('jrbank').factory('messageService',messageService);
+})();
+
