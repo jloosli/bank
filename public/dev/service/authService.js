45885e8cb288aa762bd53b46b311c6b3bd1b0db9
Jared Loosli <jloosli@gmail.com>
2014-07-31 15:23:13 -0600

    Removed comment and make sure to delete current user when token gets deleted

--- public/dev/service/authService.js
+++ public/dev/service/authService.js
@@ -29,7 +29,6 @@
             "use strict";
             localStorage.removeItem('auth_token');
             localStorage.removeItem('current_user');
-            console.log("Cleared token");
             this.init();
         };
 
