0cf69b41aa6b011a778c21a5801c7bd8e06293e3
Jared Loosli <jloosli@gmail.com>
2014-09-29 15:34:01 -0600

    Added switch-account directive

---
+++ public/dev/directive/switch-account/switch-account.js
@@ -0,0 +1,19 @@
+angular.module('jrbank').directive('switchAccount', function(banksService) {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+            current: '@'
+		},
+		templateUrl: 'directive/switch-account/switch-account.html',
+		link: function(scope, element, attrs, fn) {
+            scope.accountHolders = [];
+            banksService.users().get().$promise.then(function(results) {
+                console.log(results.users);
+                scope.otherAccountHolders = _.filter(results.users, function(user) {
+                    return user.id !== parseInt(scope.current);
+                });
+            });
+		}
+	};
+});
