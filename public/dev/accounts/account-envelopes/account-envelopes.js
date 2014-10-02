d0b4aa13c1e8471aa522c3552cb5bf0985a67b39
Jared Loosli <jloosli@gmail.com>
2014-10-02 16:59:11 -0600

    Updated envelopes

--- public/dev/accounts/account-envelopes/account-envelopes.js
+++ public/dev/accounts/account-envelopes/account-envelopes.js
@@ -3,10 +3,35 @@
      @ngInject
      */
 
-function AccountEnvelopesCtrl($scope) {
-    this.isActive = function (id) {
+function AccountEnvelopesCtrl($scope, $stateParams, $q,  banksService) {
+    var params = $stateParams,
+        self = this;
 
-    }
+    this.envelopes = {default: '', data:[]};
+
+    banksService.users(params.id).get().$promise.then(function(results) {
+        if(results.users.length) {
+            self.envelopes.data = results.users[0].envelopes;
+        }
+    });
+
+    banksService.defaultEnvelope(params.id).then(function (env) {
+        self.envelopes['default'] = env.id;
+    });
+
+    this.add = function() {
+        var env = {
+            name:'New Envelope',
+            "deleted_at": null,
+            balance: 0
+        };
+        self.envelopes.data.push(env);
+    };
+
+    this.update = function() {
+
+    };
+
 }
 
 angular.module('jrbank').controller('AccountEnvelopesCtrl', AccountEnvelopesCtrl);
--- public/dev/service/banksService.js
+++ public/dev/service/banksService.js
@@ -1,14 +1,14 @@
 (function () {
 
     /*
-    @ngInject
+     @ngInject
      */
-    function banksService ($resource, $auth, $rootScope, API_URL) {
+    function banksService($resource, $auth, $rootScope, $q, API_URL) {
         "use strict";
         var svc = {};
 
-        function currentBank () {
-            if($auth.isAuthenticated()) {
+        function currentBank() {
+            if ($auth.isAuthenticated()) {
                 var user = $rootScope.currentUser;
                 return user.bank_id;
             }
@@ -16,26 +16,45 @@
         }
 
         svc.bank = function () {
-            return $resource(API_URL + ':bank_id');
+            return $resource(API_URL + ':bank_id', {cache: true});
         };
 
         svc.users = function (user_id) {
             return $resource(API_URL + 'banks/' + currentBank() + '/users/:user_id', {
-                "user_id": user_id || ''
+                "user_id": user_id || '',
+                cache: true
             });
         };
 
-        svc.transactions = function(user_id) {
-            return $resource(API_URL  + 'banks/' + currentBank() + '/users/:user_id/transactions', {
-                "user_id": user_id
+        svc.transactions = function (user_id) {
+            return $resource(API_URL + 'banks/' + currentBank() + '/users/:user_id/transactions', {
+                "user_id": user_id,
+                cache:     true
             });
         };
 
         svc.envelopes = function (user_id) {
-            return $resource(API_URL + currentBank() + '/users/' + user_id + '/envelopes/:envelope_id');
+            return $resource(API_URL + currentBank() + '/users/' + user_id + '/envelopes/:envelope_id', {
+                "envelope_id": '',
+                cache:         true
+            });
+        };
+
+        svc.defaultEnvelope = function (user_id) {
+            var deferred = $q.defer();
+            svc.users(user_id).get().$promise.then(function (results) {
+                var envelopes = results.users[0].envelopes;
+                var default_envelope = _.find(envelopes, function (env) {
+                    return env.default_spend === '1';
+                });
+                deferred.resolve(default_envelope);
+            });
+
+            return deferred.promise;
         };
 
         return svc;
     }
-    angular.module('jrbank').factory('banksService',banksService);
+
+    angular.module('jrbank').factory('banksService', banksService);
 })();
