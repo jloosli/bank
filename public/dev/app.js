fce1b30390f28f8363b45fd48567e2fe69527c8c
Jared Loosli <jloosli@gmail.com>
2015-01-20 11:54:08 -0700

    Updated Analytics to track pageviews better

--- public/dev/app.js
+++ public/dev/app.js
@@ -217,6 +217,16 @@
             }
         });
 
+        $rootScope
+            .$on('$stateChangeSuccess',
+            function(event){
+
+                if (!$window.ga)
+                    return;
+
+                $window.ga('send', 'pageview', { page: $location.path() });
+            });
+
 
         $rootScope.safeApply = function (fn) {
             var phase = $rootScope.$$phase;
