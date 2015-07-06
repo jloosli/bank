9f3b561afc4eac4b95cffea02ded5fd7c03cca43
Jared Loosli <jloosli@gmail.com>
2015-07-06 12:04:04 -0600

    Added alerts service

---
+++ public/dev/service/alertsService-spec.js
@@ -0,0 +1,11 @@
+describe('alertsService', function() {
+
+  beforeEach(module('jrbank'));
+
+  it('should ...', inject(function(alertsService) {
+
+	//expect(alertsService.doSomething()).toEqual('something');
+
+  }));
+
+});
---
+++ public/dev/service/alertsService.js
@@ -0,0 +1,67 @@
+/* @ngInject */
+var alertsService = function ($timeout) {
+    'use strict';
+
+    var alerts = [],
+        observerCallbacks = [];
+
+    var registerObserverCallback = function(callback) {
+        observerCallbacks.push(callback);
+    };
+
+    var notifyObservers = function() {
+        angular.forEach(observerCallbacks, function(callback) {
+            callback();
+        });
+    };
+
+    var get = function () {
+        return alerts;
+    };
+
+    var add = function (alert) {
+        if (typeof alert !== 'object') {
+            alert = {
+                text: alert
+            }
+        }
+        alert.id = _.uniqueId('alert_');
+        alert.type = 'alert-' + (alert.type || 'success');
+        alerts.push(alert);
+        if (alert.duration && parseInt(alert.duration) > 0) {
+            var duration = parseInt(alert.duration) * 1000;
+            $timeout(function() {remove(alert.id);}, duration);
+        }
+        notifyObservers();
+        return alert.id;
+    };
+
+    var remove = function (id) {
+        console.log("Removing", id);
+        _.remove(alerts, function (alert) {
+            return alert.id === id;
+        });
+        notifyObservers();
+    };
+
+    var removeAll = function () {
+        alerts = [];
+        notifyObservers();
+    };
+
+    var removeNonEnduring = function() {
+        _.remove(alerts, function(alert) {
+            return !alert.endure;
+        });
+    };
+
+    return {
+        get:       get,
+        add:       add,
+        remove:    remove,
+        removeAll: removeAll,
+        registerObserverCallback: registerObserverCallback,
+        removeNonEnduring: removeNonEnduring
+    };
+};
+angular.module('jrbank').factory('alertsService', alertsService);
